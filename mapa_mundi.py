a18585e38a765dca84859387d3c474dcfe78afe3
Arthur <arthuravner@gmail.com>
2021-01-07 21:13:53 -0300

    tentativa 1 textura

--- mapa_mundi.py
+++ mapa_mundi.py
@@ -2,6 +2,7 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import * 
 from OpenGL.GL import *
 import math
+import png
 
 def desenha():
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
@@ -25,23 +26,48 @@ def getPonto(i, j):
 def desenhaEsfera():
     glBegin(GL_QUAD_STRIP)
     for i in range(0,nEsfera):
-        glColor3fv(cores[i])
+        #glColor3fv(cores[i])
         for j in range(0, nRotacao):  
             glVertex3fv(getPonto(i,j))
-            #glTexture2fv(g(i,j))
+            glTexCoord2f(i/nEsfera, j/nRotacao)
             glVertex3fv(getPonto(i+1,j))
-            #glTexture2fv(g(i+1,j))
+            glTexCoord2f((i+1)/nEsfera, j/nRotacao)
             glVertex3fv(getPonto(i,j+1))
-            #glTexture2fv(g(i,0))
+            glTexCoord2f(i/nEsfera, (j+1)/nRotacao)
             glVertex3fv(getPonto(i+1,j+1))
-            #glTexture2fv(g(i+1,0))            
+            glTexCoord2f((i+1)/nEsfera, (j+1)/nRotacao) 
+                   
     glEnd()
 
-# PROGRAMA PRINCIPAL
-cores = ( (1,0,0),(1,1,0),(0,1,0),(0,1,1),(0,0,1),(1,0,1),(0.5,1,1),(1,0,0.5) )
+def LoadTextures():
+    global texture
+    texture = glGenTextures(1)
 
-nEsfera = 8
-nRotacao = 25
+    glBindTexture(GL_TEXTURE_2D, texture)
+    reader = png.Reader(filename='mapa.png')
+    w, h, pixels, metadata = reader.read_flat()
+    if(metadata['alpha']):
+        modo = GL_RGBA
+    else:
+        modo = GL_RGB
+    glPixelStorei(GL_UNPACK_ALIGNMENT,1)
+    glTexImage2D(GL_TEXTURE_2D, 0, modo, w, h, 0, modo, GL_UNSIGNED_BYTE, pixels.tolist())
+#    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
+#    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
+
+
+
+
+# PROGRAMA PRINCIPAL
+#cores = ( (1,0,0),(1,1,0),(0,1,0),(0,1,1),(0,0,1),(1,0,1),(0.5,1,1),(1,0,0.5) )
+
+nEsfera = 50 #i
+nRotacao = 50 #j
 r = 2
 
 glutInit(sys.argv)
@@ -52,8 +78,20 @@ glutDisplayFunc(desenha)
 glEnable(GL_MULTISAMPLE)
 glEnable(GL_DEPTH_TEST)
 glClearColor(0.,0.,0.,1.)
+
+glClearDepth(1.0)
+glDepthFunc(GL_LESS)   
+glEnable(GL_DEPTH_TEST)     
+glShadeModel(GL_SMOOTH)   
+glMatrixMode(GL_PROJECTION)
+
 gluPerspective(45,800.0/600.0,0.1,50.0)
+
+glMatrixMode(GL_MODELVIEW)
+
 glTranslatef(0.0,0.0,-8)
 glRotatef(45,1,1,1)
 glutTimerFunc(50,timer,1)
+LoadTextures()
+glEnable(GL_TEXTURE_2D)
 glutMainLoop()
